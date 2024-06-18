280e2e56e1e063c829c8de51f25ba06d535ad68d
Efrain Mancilla <hastydc@gmail.com>
2024-06-18 14:45:18 -0500

    feat: 🎸 table layout

---
+++ src/app/shared/components/table/components/detail-button/detail-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetailButtonComponent } from './detail-button.component';
+
+describe('DetailButtonComponent', () => {
+  let component: DetailButtonComponent;
+  let fixture: ComponentFixture<DetailButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DetailButtonComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DetailButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Comic } from '@app/models/comic.interface';
 import { TranslateModule } from '@ngx-translate/core';
-import { DetailComponent } from './detail/detail.component';
-import { ButtonComponent } from '../button/button.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { DetailButtonComponent } from './components/detail-button/detail-button.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [TranslateModule, DetailComponent, ButtonComponent],
+  imports: [TranslateModule, DetailComponent, DetailButtonComponent, NgClass],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
@@ -16,9 +17,16 @@ export class TableComponent {
 
   @Input() comics: Comic[] = [];
 
+  actives: { [key: string]: boolean } = {};
+
   toggleFavorite(comic: Comic): void {
     this.eventFavorite.emit(comic);
   }
 
-  toggleDetail(id: number): void {}
+  toggleDetail(id: number): void {
+    this.actives = {
+      ...this.actives,
+      [id.toString()]: !this.actives[id],
+    };
+  }
 }
